18f236446fd7ef427da2af6816917f515d912366
arcticviper <charles2004922@gmail.com>
2016-06-29 23:33:24 +1000

    29/06 - 3
    Complete setup

--- home.py
+++ home.py
@@ -2,50 +2,40 @@
 # Version 0.1
 # Created by Charles Denison
 # Setup
+# Potentially try or give option to use sqlite or MYSQL in future iterations
 import sqlite3
-def Setup():
-        cur.excute("INSERT INTO users VALUES()")
-        con.commit()
-
-# Open database connection
-db = conn.connect("localhost","testuser","test123","TESTDB" )
-
+#define main setup
+def Main():
+        try:
+# Initialisation/connection
+                conn = sqlite3.connect('setup.db')
 # prepare a cursor object using cursor() method
-cursor = db.cursor()
+                c = conn.cursor()
 
-# execute SQL query using execute() method.
-cursor.execute("SELECT VERSION()")
+#table setup for login and items
+                c.execute('CREATE TABLE IF NOT EXISTS users(ID KEY, Email TEXT,password TEXT)') #create login table email+password
+                c.execute('CREATE TABLE IF NOT EXISTS users(ID KEY, ItemName TEXT, Category TEXT, Quantity INT, Notes TEXT, Value INT, Total REAL)') #create item table of ID, ItemName, Category, Quanitty, notes, value, total)
 
-# Fetch a single row using fetchone() method.
-data = cursor.fetchone()
+# inserting test data, add # when done testing
+                c.execute("INSERT INTO users VALUES(2, 'name@albertparkcollege.vic.edu.au', 'APC00000')")
 
-print ("Database version : %s " % data)
+# grabbing and checking data
+                c.execute("SELECT * FROM Users")
+               
+                data = c.fetchall()
 
+                for row in data:
+                        print (row)
+# If error occurs, print notice
+        except sqlite3.Error:
+                if conn:
+                        conn.rollback()
+                        print ("There was a problem with the SQL")
 # disconnect from server
-db.close()
-
-try:
-        con = sqlite3.connect('setup.db')
-        cur = con.cursor()
-        cur.execute('CREATE TABLE users(Email TEXT,password TEXT)') #create login table email+password
-        cur.execute('CREATE TABLE items(ID KEY, ItemName TEXT, Category TEXT, Quantity INT, Notes TEXT, Value INT, Total REAL)') #create item table of ID, ItemName, Category, Quanitty, notes, value, total)
-        cur.execute("INSERT INTO users VALUES('FirstnameLastname', 'APC00000')")#intesting test data
-
-        con.commit() #save changes
-                
-        cur.execute("SELECT * FROM Users") #
-                
-        data = cur.fetchall()
-
-        for row in data:
-                print (row)
-
-except sqlite3.Error:
-        if con:
-                con.rollback()
-                print ("There was a problem with the SQL")
-finally:
-        if con:
-                con.close()
+        finally:
+                if conn:
+                        conn.commit()
+                        conn.close()
+# execute 
 if __name__ == '__main__':
 	Main()
