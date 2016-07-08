e08c1d12ca15dcc2c655a0b738e458bdf7d5ca63
arcticviper <charles2004922@gmail.com>
2016-07-09 00:18:13 +1000

    9/07
    Added user interface to login program.
    Added admin panel interface.

---
+++ adminpanel.py
@@ -0,0 +1,51 @@
+# BILS - BASIC ITEM LOGGING SOFTWARE
+# Version 0.1
+# Created by Charles Denison
+# ADMIN PANEL
+from tkinter import *
+import tkinter
+import os
+
+
+class Application(Frame):
+    def __init__(self,master):
+        Frame.__init__(self,master)
+        self.grid()
+        self.create_widget()
+
+    def create_widget(self):
+        self.bttn1 = Button(root,text="Item Add",width=80,bg="#737373")
+        #self.bttn1["command"] = #open item add
+        self.bttn1.grid(row=1,column=0)
+        self.bttn2 = Button(root,text="Item Modify",width=80,bg="#737373")
+        #self.bttn2["command"] = #open item modify
+        self.bttn2.grid(row=2,column=0)
+        self.bttn3 = Button(root,text="User Add",width=80,bg="#737373")
+        #self.bttn3["command"] = #open item modify
+        self.bttn3.grid(row=3,column=0)
+        self.bttn4 = Button(root,text="User Modify",width=80,bg="#737373")
+        #self.bttn4["command"] = #open item modify
+        self.bttn4.grid(row=4,column=0)
+        self.bttn5 = Button(root,text="User Panel",width=80,bg="#737373")
+        #self.bttn5["command"] = #open item modify
+        self.bttn5.grid(row=5,column=0)
+        self.bttn6 = Button(root,text="User Loging",width=80,bg="#737373")
+        #self.bttn6["command"] = #open item modify
+        self.bttn6.grid(row=6,column=0)
+        self.bttn7 = Button(root,text="Item Loging",width=80,bg="#737373")
+        #self.bttn7["command"] = #open item modify
+        self.bttn7.grid(row=7,column=0)
+        self.bttn8 = Button(root,text="Logout",width=80,bg="#737373")
+        self.bttn8["command"] = #
+        self.bttn8.grid(row=8,column=0)
+        self.bttn9 = Button(root,text="Quit",width=80,bg="#737373")
+        self.bttn9["command"] = quit
+        self.bttn9.grid(row=9,column=0)
+
+root = Tk()
+root.configure(bg="#707070")
+logo = PhotoImage(file="APC-logo.png")
+w1 = Label(root, image=logo).grid(row=0,column=0)
+#root.geometry("100x250") not required
+app = Application(root)
+root.mainloop        
--- login.py
+++ login.py
@@ -2,32 +2,53 @@
 # Version 0.1
 # Created by Charles Denison
 # LOGIN
+from tkinter import *
+import tkinter.messagebox as tm
 import sqlite3
 import getpass
 import time
 import datetime
-attempt = 1
 conn = sqlite3.connect('setup.db')
 c = conn.cursor()
 unix = time.time()
 date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%M-%d %H:%M:%S'))
 #user input
-user = input ("User: ")
+class Login(Frame): #create loginframe
+    def __init__(self, master):
+        super().__init__(master)#inherit base class
+        #init labels
+        self.labelusr = Label(self, text="Username")
+        self.labelpw = Label(self, text="Password")
+        #init entry
+        self.entryusr = Entry(self)
+        self.entrypw = Entry(self, show="*")
 
-passwordio = getpass.getpass(prompt='Password: ', stream=None)
-#read from users table and ensures that user and password matches with information on database
-#when using different tables ensure that Email  and password matches the table selected (users) and only selecting users who have under 3 incorrect attempts
-c.execute('SELECT * from users WHERE Email="%s" AND password="%s" AND unattempt<3' % (user, passwordio))
-Mydata = c.execute('SELECT TextString from ControlTable WHERE Group = "Messages" and GroupNum = 1)')
-if c.fetchone() is not None:
-    print ("Welcome")
-    c.execute("INSERT INTO userlog(Attempt, Email, datestamp,sucessful)VALUES(?,?,?,?)",(attempt,user,date,True))
-    c.execute('UPDATE users SET unattempt = 0 WHERE Email= ?',(user,))
-    conn.commit()
-else:
-    attempt=attempt+1
-    print ("Login failed, please ensure you have typed your details correctly. Otherwise please contact the system adminstrator")
-    c.execute("INSERT INTO userlog(Attempt, Email, datestamp,sucessful)VALUES(?,?,?,?)",(attempt,user,date,False))
-    c.execute('UPDATE users SET unattempt = unattempt+1 WHERE Email= ?',(user,))
-    print (Mydata)
-    conn.commit()
+        self.labelusr.grid(row=0, sticky=E)
+        self.labelpw.grid(row=1, sticky=E)
+        self.entryusr.grid(row=0, column=1)
+        self.entrypw.grid(row=1, column=1)
+        self.logbtn = Button(self, text="Login", command = self._login_btn_clickked)
+        self.logbtn.grid(columnspan=2)
+        self.pack() #pack grid
+    def _login_btn_clickked(self):
+        user = self.entryusr.get()
+        password = self.entrypw.get()
+        attempt = 1
+        #read from users table and ensures that user and password matches with information on database
+        #when using different tables ensure that Email  and password matches the table selected (users) and only selecting users who have under 3 incorrect attempts
+        c.execute('SELECT * from users WHERE Email="%s" AND password="%s" AND unattempt<3' % (user, password))
+        #Mydata = c.execute('SELECT TextString from ControlTable WHERE Group = "Messages" and GroupNum = 1)')
+        if c.fetchone() is not None:
+            tm.showinfo("Login info", "Welcome")
+            c.execute("INSERT INTO userlog(Attempt, Email, datestamp,sucessful)VALUES(?,?,?,?)",(attempt,user,date,True))
+            c.execute('UPDATE users SET unattempt = 0 WHERE Email= ?',(user,))
+            conn.commit()
+        else:
+            attempt=attempt+1
+            tm.showerror("Login error", "Login failed, please ensure you have typed your details correctly. Otherwise please contact the system adminstrator")
+            c.execute("INSERT INTO userlog(Attempt, Email, datestamp,sucessful)VALUES(?,?,?,?)",(attempt,user,date,False))
+            c.execute('UPDATE users SET unattempt = unattempt+1 WHERE Email= ?',(user,))
+            conn.commit()
+root = Tk()
+lf = Login(root)
+root.mainloop()
