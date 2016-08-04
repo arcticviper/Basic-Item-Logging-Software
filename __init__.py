4f674ec80400e1847da37a825736511c66850a92
arcticviper <charles2004922@gmail.com>
2016-08-04 23:21:58 +1000

    4/08 - 3
    Added init file

---
+++ __init__.py
@@ -0,0 +1,5 @@
+#Init file
+__all__=['adminadduser','adminitemadd','adminitemmod',
+         'adminlogitem','adminloguser','adminmoduser',
+         'adminpanel','loading','login','userborrow',
+         'userpanel','userreserve','userreturn']
