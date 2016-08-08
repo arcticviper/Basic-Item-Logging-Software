26cc9616d62dbb1dfe6f5e38352507265ecfb357
arcticviper <charles2004922@gmail.com>
2016-08-09 00:19:40 +1000

    8/08
    Redesigned user interface for item and user log

--- adminloguser.py
+++ adminloguser.py
@@ -28,33 +28,53 @@ class Borrow(Frame): #create returnframe
         self.labeluser = Label(self, text="Username")
         self.labeldate = Label(self, text="Date Stamp")
         self.labelunat = Label(self, text="Successful")
-        self.labelfull = Label(self, text="Full Info")
-        #init entry
+        self.labellgid = Label(self, text="Log ID")
+        self.labelincr2 = Label(self, text="Wrong \n attempts")
+        self.labeluser2 = Label(self, text="Username")
+        self.labeldate2 = Label(self, text="Date Stamp")
+        self.labelunat2 = Label(self, text="Successful")
+        self.labellgid2 = Label(self, text="Log ID")
+        self.labeldivd = Label(self, text="______________________________________________________________________________")
+        #init text boxes
         self.entryincr = Entry(self)
         self.entryuser = Entry(self)
         self.entrydate = Entry(self)
         self.entryunat = Entry(self)
-        self.textfull = Text(self, width= 60, height = 4)
+        self.textlgid = Text(self, height = 6, width = 7) #id
+        self.textincr = Text(self, height = 6, width = 7) #inc att
+        self.textuser = Text(self, height = 6, width = 20)#usr
+        self.textdate = Text(self, height = 6, width = 20)#date
+        self.textatmp = Text(self, height = 6, width = 7)#successful
+
         #grid sorting
-        self.labelincr.grid(row=1, sticky=E)
-        self.labeluser.grid(row=2, sticky=E)
-        self.labeldate.grid(row=3, sticky=E)
-        self.labelunat.grid(row=4, sticky=E)
-        self.labelfull.grid(row=5, column=1)
-        self.entryincr.grid(row=1, column=1)
-        self.entryuser.grid(row=2, column=1)
-        self.entrydate.grid(row=3, column=1)
-        self.entryunat.grid(row=4, column=1)
-        self.textfull.grid(row=6, column=0,columnspan=3,rowspan=4)
-        self.textfull.grid_columnconfigure(1, weight=1)
+        self.labelincr.grid(row=1, column=1)
+        self.labeluser.grid(row=2, column=1)
+        self.labeldate.grid(row=3, column=1)
+        self.labelunat.grid(row=4, column=1)
+        self.entryincr.grid(row=1, column=2)
+        self.entryuser.grid(row=2, column=2)
+        self.entrydate.grid(row=3, column=2)
+        self.entryunat.grid(row=4, column=2)
+        self.labeldivd.grid(row=5, column=1,columnspan=3)
+        self.labellgid2.grid(row=6, column=0)
+        self.labelincr2.grid(row=6, column=1)
+        self.labeluser2.grid(row=6, column=2)
+        self.labeldate2.grid(row=6, column=3)
+        self.labelunat2.grid(row=6, column=4)
+        self.textlgid.grid(row=7, column=0,padx=5)
+        self.textincr.grid(row=7, column=1,padx=5)
+        self.textuser.grid(row=7, column=2,padx=5)
+        self.textdate.grid(row=7, column=3,padx=5)
+        self.textatmp.grid(row=7, column=4,padx=5)
+        
         self.srcbtn = Button(self, text="Latest User Login", command = self._search_btn_clickked,bg="#B3B3B3",width = 15)
-        self.srcbtn.grid(row=1, column=2)
+        self.srcbtn.grid(row=1, column=3)
         self.srcbtn = Button(self, text="Full User Search", command = self._fullsearch_btn_clickked,bg="#B3B3B3",width = 15)
-        self.srcbtn.grid(row=2, column=2)
+        self.srcbtn.grid(row=2, column=3)
         self.modbtn = Button(self, text="Clear", command = self._clear_btn_clickked,bg="#B3B3B3",width = 15)
-        self.modbtn.grid(row=3, column=2)
+        self.modbtn.grid(row=3, column=3)
         self.logout = Button(self, text="Exit", command = adminpanel,bg="#B3B3B3",width = 15)
-        self.logout.grid(row=4, column=2)
+        self.logout.grid(row=4, column=3)
         # frame complete
         # button functions
     def _fullsearch_btn_clickked(self):
@@ -65,7 +85,7 @@ class Borrow(Frame): #create returnframe
         self.entryincr.delete(0, 'end')
         self.entrydate.delete(0, 'end')
         self.entryunat.delete(0, 'end')
-        self.textfull.delete(0.0, 'end')
+        self.textlgid.delete(0.0, 'end')
         searchatt = c.execute('SELECT Attempt FROM userlog WHERE Email="%s"' % (user))
         result1=c.fetchone()
         print(result1)
@@ -88,9 +108,25 @@ class Borrow(Frame): #create returnframe
                     tm.showerror("format error", "Search failed, please ensure you have typed the details correctly.")
                     conn.commit()
                 self.entrydate.insert(END, getdate)
+                #use more efficient code in future?
                 for row in c.execute('SELECT * FROM userlog WHERE Email="%s"' % (user)):
-                    self.textfull.insert(END, row)
-                    self.textfull.insert(END, "\n")
+                        logid,incat,email,dates,sucess=(row)
+                        print (sucess)
+                        if sucess == 1:
+                                sucess = "Yes"
+                        else:
+                                sucess = "No"
+                        self.textlgid.insert(END, logid)
+                        self.textlgid.insert(END, "\n")
+                        self.textincr.insert(END, incat)
+                        self.textincr.insert(END, "\n")
+                        self.textuser.insert(END, email)
+                        self.textuser.insert(END, "\n")
+                        self.textdate.insert(END, dates)
+                        self.textdate.insert(END, "\n")
+                        self.textatmp.insert(END, sucess)
+                        self.textatmp.insert(END, "\n")
+                        
                 tm.showinfo("Search Found", "This user has been found in the database.")
                 conn.commit()
         else:
@@ -136,7 +172,11 @@ class Borrow(Frame): #create returnframe
         self.entrydate.delete(0, 'end')
         self.entryunat.delete(0, 'end')
         self.entrydate.delete(0, 'end')
-        self.textfull.delete(1.0, 'end')
+        self.textlgid.delete(1.0, 'end')
+        self.textincr.delete(1.0, 'end')
+        self.textuser.delete(1.0, 'end')
+        self.textdate.delete(1.0, 'end')
+        self.textatmp.delete(1.0, 'end')
         tm.showinfo("Clear input", "Input has been cleared.")
         conn.commit()
         
